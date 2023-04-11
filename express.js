bc134cc7314a4e02cd5abcb1381689c2395ca6da
Luan Kisaki <luankisaki@gmail.com>
2023-04-11 16:32:04 -0300

    use

--- express.js
+++ express.js
@@ -2,11 +2,14 @@ const express = require('express')
 
 const app = express()
 
+// app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('<h1>Lista de tarefas</h1>')
 })
 
 app.get('/json', (req, res) => {
+  // console.log(req.body)
   res.json({title: 'Tarefa', done: true})
 })
 
