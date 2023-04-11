7a5bfe34ca1cb27bcb55916d5c3f02efb712a1b4
Luan Kisaki <luankisaki@gmail.com>
2023-04-11 18:27:03 -0300

    parametros-nas-rotas

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ app.use(express.json());
 
 app.use('/checklists', checkListRouter);
 
-// const log = (req,res, next) => {
-//   console.log (`Data: ${Date.now()}`);
-//   next();
-// }
+const log = (req,res, next) => {
+  console.log (`Data: ${Date.now()}`);
+  next();
+}
 
-// app.use(log);
+app.use(log);
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Lista de tarefas</h1>');
-// })
+app.get('/', (req, res) => {
+  res.send('<h1>Lista de tarefas</h1>');
+})
 
-// app.get('/json', (req, res) => {
-//   console.log(req.body);
-//   res.json({title: 'Tarefa', done: true});
-// })
+app.get('/json', (req, res) => {
+  console.log(req.body);
+  res.json({title: 'Tarefa', done: true});
+})
 
 app.listen(3000, () => {
   console.log('Servidor foi iniciado');
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -7,4 +7,14 @@ router.get('/', (req, res) => {
   res.send();
 })
 
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(200).send(req.body);
+})
+
+router.get('/:id', (req, res) => {
+  console.log(req.params.id);
+  res.send(`ID: ${req.params.id}`)
+})
+
 module.exports = router;
