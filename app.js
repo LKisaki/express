4c2da8323150bda2ad38b8bbc2cb6f0e1785f4d1
Luan Kisaki <luankisaki@gmail.com>
2023-04-12 12:40:43 -0300

    -

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require('express')
 const checkListRouter = require('./src/routes/checklist')
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-app.use('/checklists', checkListRouter);
+app.use('/checklists', checkListRouter)
 
 const log = (req,res, next) => {
-  console.log (`Data: ${Date.now()}`);
-  next();
+  console.log (`Data: ${Date.now()}`)
+  next()
 }
 
-app.use(log);
+app.use(log)
 
 app.get('/', (req, res) => {
-  res.send('<h1>Lista de tarefas</h1>');
+  res.send('<h1>NodeJs</h1>')
 })
 
+// app.route('/').get( (req, res) => res.send('<h1>NodeJs</h1>'))
+
 app.get('/json', (req, res) => {
-  console.log(req.body);
-  res.json({title: 'Tarefa', done: true});
+  console.log(req.body)
+  res.json({title: 'Tarefa', done: true})
 })
 
 app.listen(3000, () => {
-  console.log('Servidor foi iniciado');
+  console.log('Servidor foi iniciado')
 })
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,32 +1,32 @@
-const express = require('express');
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', (req, res) => {
-  console.log('Olá');
-  res.send();
+  console.log('Olá')
+  res.send()
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body);
-  res.status(200).send(req.body);
+  console.log(req.body)
+  res.status(200).send(req.body)
 })
 
 router.get('/:id', (req, res) => {
-  console.log(req.params.id);
+  console.log(req.params.id)
   res.send(`ID: ${req.params.id}`)
 })
 
 router.put('/:id', (req, res) => {
-  console.log(req.body);
-  res.send(`PUT ID: ${req.params.id}`);
+  console.log(req.body)
+  res.send(`PUT ID: ${req.params.id}`)
 
 })
 
 router.delete('/:id', (req, res) => {
-  console.log(req.body);
-  res.send(`DELETE ID: ${req.params.id}`);
+  console.log(req.body)
+  res.send(`DELETE ID: ${req.params.id}`)
 
 })
 
-module.exports = router;
+module.exports = router
